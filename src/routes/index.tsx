e24b93c58a2a3b1f5c919705cea8ccf455253dff
Victor <victorgt2406@github.com>
2023-05-13 00:18:03 +0200

    html set up

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "./home";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>,
+        element: <Home/>,
     },
 ]);
 
