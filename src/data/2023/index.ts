be8a5acb65e6c2834575c3bcd677a12196268d8c
Victor <victorgt2406@github.com>
2023-05-13 00:48:25 +0200

    data corrected

--- src/data/2023/index.ts
+++ src/data/2023/index.ts
@@ -1,443 +1,492 @@
 import { Pilot, Porra } from "./types";
 
 
-const porraSquare:Porra[] = [
+const porraSquare: Porra[] = [
     {
         player: "square",
         position: 1,
         constructor: "red_bull",
-        pilot: "max_verstappen"
-        
+        pilot: "max_verstappen",
+        year: 2023
     },
     {
         player: "square",
         position: 2,
         constructor: "mercedes",
-        pilot: "hamilton"
-        
+        pilot: "hamilton",
+        year: 2023
     },
     {
         player: "square",
         position: 3,
         constructor: "aston_martin",
-        pilot: "alonso"
-        
+        pilot: "alonso",
+        year: 2023
     },
     {
         player: "square",
         position: 4,
         constructor: "ferrari",
-        pilot: "perez"
-        
+        pilot: "perez",
+        year: 2023
     },
     {
         player: "square",
         position: 5,
         constructor: "mclaren",
-        pilot: "leclerc"
-        
+        pilot: "leclerc",
+        year: 2023
     },
     {
         player: "square",
         position: 6,
         constructor: "alpine",
-        pilot: "russell"
-        
+        pilot: "russell",
+        year: 2023
     },
     {
         player: "square",
         position: 7,
         constructor: "alfa",
-        pilot: "sainz"
-        
+        pilot: "sainz",
+        year: 2023
     },
     {
         player: "square",
         position: 8,
         constructor: "alphatauri",
         pilot: "stroll"
-        
+
+        , year: 2023
     },
     {
         player: "square",
         position: 9,
         constructor: "haas",
-        pilot: "norris"
-        
+        pilot: "norris",
+        year: 2023
     },
     {
         player: "square",
         position: 10,
         constructor: "williams",
-        pilot: "ocon"
-        
+        pilot: "ocon",
+        year: 2023
     },
     {
         player: "square",
         position: 11,
         constructor: undefined,
-        pilot: "gasly"
-        
+        pilot: "gasly",
+        year: 2023
     },
     {
         player: "square",
         position: 12,
         constructor: undefined,
-        pilot: "bottas"
-        
+        pilot: "bottas",
+        year: 2023
     },
     {
         player: "square",
         position: 13,
         constructor: undefined,
-        pilot: "piastri"
-        
+        pilot: "piastri",
+        year: 2023
     },
     {
         player: "square",
         position: 14,
         constructor: undefined,
         pilot: "zhou"
-        
+
+        , year: 2023
     },
     {
         player: "square",
         position: 15,
         constructor: undefined,
         pilot: "kevin_magnussen"
-        
+
+        , year: 2023
     },
     {
         player: "square",
         position: 16,
         constructor: undefined,
         pilot: "de_vries"
-        
+
+        , year: 2023
     },
     {
         player: "square",
         position: 17,
         constructor: undefined,
         pilot: "albon"
-        
+
+        , year: 2023
     },
     {
         player: "square",
         position: 18,
         constructor: undefined,
         pilot: "hulkenberg"
-        
+
+        , year: 2023
     },
     {
         player: "square",
         position: 19,
         constructor: undefined,
         pilot: "tsunoda"
-        
+
+        , year: 2023
     },
     {
         player: "square",
         position: 20,
         constructor: undefined,
         pilot: "sargeant"
-        
+
+        , year: 2023
     },
 
 ];
 
 
-const porraDax:Porra[] = [
+const porraDax: Porra[] = [
     {
         player: "dax",
         position: 1,
         constructor: "red_bull",
         pilot: "alonso"
-        
+
+        , year: 2023
     },
     {
         player: "dax",
         position: 2,
         constructor: "aston_martin",
         pilot: "hamilton"
-        
+
+        , year: 2023
     },
     {
         player: "dax",
         position: 3,
         constructor: "ferrari",
         pilot: "max_verstappen"
-        
+
+        , year: 2023
     },
     {
         player: "dax",
         position: 4,
         constructor: "mercedes",
         pilot: "leclerc"
-        
+
+        , year: 2023
     },
     {
         player: "dax",
         position: 5,
         constructor: "mclaren",
         pilot: "russell"
-        
+
+        , year: 2023
     },
     {
         player: "dax",
         position: 6,
         constructor: "alfa",
         pilot: "sainz"
-        
+
+        , year: 2023
     },
     {
         player: "dax",
         position: 7,
         constructor: "alpine",
         pilot: "perez"
-        
+
+        , year: 2023
     },
     {
         player: "dax",
         position: 8,
         constructor: "haas",
         pilot: "norris"
-        
+
+        , year: 2023
     },
     {
         player: "dax",
         position: 9,
         constructor: "alphatauri",
         pilot: "ocon"
-        
+
+        , year: 2023
     },
     {
         player: "dax",
         position: 10,
         constructor: "williams",
         pilot: "stroll"
-        
+
+        , year: 2023
     },
     {
         player: "dax",
         position: 11,
         constructor: undefined,
         pilot: "gasly"
-        
+
+        , year: 2023
     },
     {
         player: "dax",
         position: 12,
         constructor: undefined,
         pilot: "bottas"
-        
+
+        , year: 2023
     },
     {
         player: "dax",
         position: 13,
         constructor: undefined,
         pilot: "de_vries"
-        
+
+        , year: 2023
     },
     {
         player: "dax",
         position: 14,
         constructor: undefined,
         pilot: "piastri"
-        
+
+        , year: 2023
     },
     {
         player: "dax",
         position: 15,
         constructor: undefined,
         pilot: "hulkenberg"
-        
+
+        , year: 2023
     },
     {
         player: "dax",
         position: 16,
         constructor: undefined,
         pilot: "kevin_magnussen"
-        
+
+        , year: 2023
     },
     {
         player: "dax",
         position: 17,
         constructor: undefined,
         pilot: "vettel" as Pilot
-        
+
+        , year: 2023
     },
     {
         player: "dax",
         position: 18,
         constructor: undefined,
         pilot: "albon"
-        
+
+        , year: 2023
     },
     {
         player: "dax",
         position: 19,
         constructor: undefined,
         pilot: "zhou"
-        
+
+        , year: 2023
     },
     {
         player: "dax",
         position: 20,
         constructor: undefined,
         pilot: "tsunoda"
-        
+
+        , year: 2023
     },
     {
         player: "dax",
         position: 21,
         constructor: undefined,
         pilot: "sargeant"
-        
+
+        , year: 2023
     },
 
 ];
 
 
-const porraBic:Porra[] = [
+const porraBic: Porra[] = [
     {
         player: "bic",
         position: 1,
         constructor: "red_bull",
         pilot: "max_verstappen"
-        
+
+        , year: 2023
     },
     {
         player: "bic",
         position: 2,
         constructor: "ferrari",
         pilot: "leclerc"
-        
+
+        , year: 2023
     },
     {
         player: "bic",
         position: 3,
         constructor: "mercedes",
         pilot: "sainz"
-        
+
+        , year: 2023
     },
     {
         player: "bic",
         position: 4,
         constructor: "aston_martin",
         pilot: "perez"
-        
+
+        , year: 2023
     },
     {
         player: "bic",
         position: 5,
         constructor: "alpine",
         pilot: "alonso"
-        
+
+        , year: 2023
     },
     {
         player: "bic",
         position: 6,
         constructor: "mclaren",
         pilot: "hamilton"
-        
+
+        , year: 2023
     },
     {
         player: "bic",
         position: 7,
         constructor: "alfa",
         pilot: "russell"
-        
+
+        , year: 2023
     },
     {
         player: "bic",
         position: 8,
         constructor: "williams",
         pilot: "stroll"
-        
+
+        , year: 2023
     },
     {
         player: "bic",
         position: 9,
         constructor: "alphatauri",
         pilot: "norris"
-        
+
+        , year: 2023
     },
     {
         player: "bic",
         position: 10,
         constructor: "haas",
         pilot: "ocon"
-        
+
+        , year: 2023
     },
     {
         player: "bic",
         position: 11,
         constructor: undefined,
         pilot: "gasly"
-        
+
+        , year: 2023
     },
     {
         player: "bic",
         position: 12,
         constructor: undefined,
         pilot: "piastri"
-        
+
+        , year: 2023
     },
     {
         player: "bic",
         position: 13,
         constructor: undefined,
         pilot: "bottas"
-        
+
+        , year: 2023
     },
     {
         player: "bic",
         position: 14,
         constructor: undefined,
         pilot: "albon"
-        
+
+        , year: 2023
     },
     {
         player: "bic",
         position: 15,
         constructor: undefined,
         pilot: "zhou"
-        
+
+        , year: 2023
     },
     {
         player: "bic",
         position: 16,
         constructor: undefined,
         pilot: "de_vries"
-        
+
+        , year: 2023
     },
     {
         player: "bic",
         position: 17,
         constructor: undefined,
         pilot: "tsunoda"
-        
+
+        , year: 2023
     },
     {
         player: "bic",
         position: 18,
         constructor: undefined,
         pilot: "kevin_magnussen"
-        
+
+        , year: 2023
     },
     {
         player: "bic",
         position: 19,
         constructor: undefined,
         pilot: "sargeant"
-        
+
+        , year: 2023
     },
     {
         player: "bic",
         position: 20,
         constructor: undefined,
         pilot: "hulkenberg"
-        
+
+        , year: 2023
     },
 
 ];
