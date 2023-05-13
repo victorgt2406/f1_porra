5a816af20ea83ef756d5afe72542178d1ee20250
Victor <victorgt2406@github.com>
2023-05-13 20:43:49 +0200

    ergast

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,22 @@
+import axios from "axios";
 import data from "../data";
+import { useEffect, useState } from "react";
 
 export default function () {
+
+    useEffect(() => {
+        const getStandings = async () => {
+            try {
+                const standings = await axios.get("http://ergast.com/api/f1/2023/driverStandings.json");
+                console.log(standings.data);
+            }
+            catch (err) {
+                console.log("error loading", err);
+            }
+        }
+        getStandings();
+    });
     return (
-        <div>{ data.map((value)=>{return <div>{value.pilot}</div>}) }</div>
+        <div>{data.map((value) => { return <div>{value.pilot}</div> })}</div>
     );
 }
